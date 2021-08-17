6a8ce281d0174b06e0ae8c6ce50119d97a4a0347
Alec Magnet <amagnet@gmail.com>
2021-08-16 21:40:47 -0400

    commit tree aa1dbee9cdc32ead956febde64eb5f40714cfca4

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,17 @@
 class Show < ActiveRecord::Base
 	has_many :characters
 	has_many :actors, through: :characters
+	belongs_to :network
+
+	def self.characters
+		self.characters
+	end
+
+	# def network
+	# 	self.network_id
+	# end
+
+	def actors_list
+		self.characters.map {|c| c.actor.full_name}
+	end
 end
